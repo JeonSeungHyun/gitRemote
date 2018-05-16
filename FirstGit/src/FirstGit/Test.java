fbad04a07ef27e44ddaf617e1dadd277ec0072d0
User <User@DESKTOP-3T3LBOD>
2018-05-16 11:10:24 +0900

    Initial commit from local

---
+++ FirstGit/src/FirstGit/Test.java
@@ -0,0 +1,11 @@
+package FirstGit;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Jeon Seung Hyun");
+		System.out.println('Àü');
+		System.out.println("Àü½ÂÇö");
+	}
+
+}
---
+++ FirstGit/src/FirstGit/Test1.java
@@ -0,0 +1,9 @@
+package FirstGit;
+
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("Jeon Seung Hyun");
+
+}
+
+}
